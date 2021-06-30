1080624ff132b3fd17d9050a5d64347bdab77c95
Martin Diaz <mdiaz@citylendinginc.com>
2021-06-29 20:41:42 -0500

    Actualizacion

--- TypeScript/Clase0623/app.ts
+++ TypeScript/Clase0623/app.ts
@@ -1,18 +1,23 @@
-"use strict";
 (() => {
-    const retirarDinero = (monto) => {
+
+    const retirarDinero = (monto: number) => {
+        
         let miDinero = 10000;
         return new Promise((res, rej) => {
             if (monto > miDinero) {
                 rej("No hay fondos suficientes en tu cuenta");
-            }
-            else {
+            } else {
                 miDinero -= monto;
                 res(miDinero);
-            }
+           } 
         });
-    };
+    }
+
+         
     retirarDinero(10990)
-        .then(miDinero => console.log(`Te quedan ${miDinero}`))
-        .catch(console.warn);
-})();
+    .then(miDinero => console.log(`Te quedan ${miDinero}`))
+    .catch(console.warn);
+
+}
+)();
+
